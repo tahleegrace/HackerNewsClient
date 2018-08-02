9648d80d7acbd73db7d3b436a0b47979dceb34e3
Andrew Ling <andrew.ling89@gmail.com>
2018-08-02 15:44:49 +0800

    Added support for searching for articles.

--- hacker-news-client/src/app/app.component.ts
+++ hacker-news-client/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from "@angular/forms";
+import { debounceTime, distinctUntilChanged, switchMap } from "rxjs/operators";
 
 import { HackerNewsService } from "./service/hacker-news.service";
 
@@ -8,14 +10,19 @@ import { HackerNewsService } from "./service/hacker-news.service";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  public queryField: FormControl = new FormControl();
+
   public results: ISearchResult[];
 
   constructor(private hackerNewsService: HackerNewsService) { }
 
   ngOnInit() {
-    // TODO: Test code only.
-    this.hackerNewsService.getSearchResults('foo', 1, 50).subscribe((result) => {
-      this.results = result.hits;
-    });
+    this.queryField.valueChanges.pipe(
+      debounceTime(200),
+      distinctUntilChanged(),
+      switchMap((query: string) => this.hackerNewsService.getSearchResults(query, 1, 50)))
+      .subscribe((result) => {
+        this.results = result.hits;
+      });
   }
 }
--- hacker-news-client/src/app/app.module.ts
+++ hacker-news-client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from "@angular/common/http";
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { HackerNewsService } from "./service/hacker-news.service";
 
@@ -14,7 +15,8 @@ import { SearchResultsComponent } from './search-results/search-results.componen
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [
     HackerNewsService
