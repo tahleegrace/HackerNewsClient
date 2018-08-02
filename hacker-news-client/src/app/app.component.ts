955362510422e709634f9c5f9c36792386fd51f8
Andrew Ling <andrew.ling89@gmail.com>
2018-08-02 15:22:12 +0800

    Completed search results component.

--- hacker-news-client/src/app/app.component.ts
+++ hacker-news-client/src/app/app.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { HackerNewsService } from "./service/hacker-news.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public results: ISearchResult[];
+
+  constructor(private hackerNewsService: HackerNewsService) { }
+
+  ngOnInit() {
+    // TODO: Test code only.
+    this.hackerNewsService.getSearchResults('foo', 1, 50).subscribe((result) => {
+      this.results = result.hits;
+    });
+  }
 }
--- hacker-news-client/src/app/search-results/search-results.component.ts
+++ hacker-news-client/src/app/search-results/search-results.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-search-results',
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-results.component.scss']
 })
 export class SearchResultsComponent implements OnInit {
+  @Input()
+  public results: ISearchResult[];
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
